020afb038d2f98d2f4fe22285f22fd5afafd4d24
Johannes <jkbecker@bu.edu>
2020-07-09 10:12:05 -0400

    convert tabs to spaces

--- pirrigotchi/__main__.py
+++ pirrigotchi/__main__.py
@@ -1,4 +1,4 @@
 import pirrigotchi
 
 if __name__ == '__main__':
-	pirrigotchi.__main__()
+    pirrigotchi.__main__()
--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from pathlib import Path
 conn = sqlite3.connect(os.sep.join([str(Path.home()), '.pirrigotchi-data.sqlite']))
 
 conn.execute('''CREATE TABLE data (
-		_id integer primary key autoincrement,
-		timestamp date default CURRENT_TIMESTAMP,
-		soiltemp real, 
-		soilhum real, 
-		lum real, 
-		airtemp real, 
-		sensor text
-	)''')
+        _id integer primary key autoincrement,
+        timestamp date default CURRENT_TIMESTAMP,
+        soiltemp real, 
+        soilhum real, 
+        lum real, 
+        airtemp real, 
+        sensor text
+    )''')
